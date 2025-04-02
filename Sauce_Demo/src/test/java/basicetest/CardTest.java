5367a22cb24404874bc0ed0683cce72af5fd9976
vimalkaveri <vimalvetri.k@gmail.com>
2025-04-02 21:53:49 +0530

    add card test case

--- Sauce_Demo/src/test/java/basicetest/CardTest.java
+++ Sauce_Demo/src/test/java/basicetest/CardTest.java
@@ -9,9 +9,13 @@ public class CardTest extends CardModule{
 	@Test(priority=1, description="to verfiy Add Card button ")
 	public void TC_AC_001() {
 		addCard(1);
+		cardBadge();
+	}
+	@Test(priority=2, description="to verfiy multiple item Add Card button ")
+	public void TC_AC_002() {
+		addCard(1);
 		addCard(2);
 		addCard(3);
 		cardBadge();
 	}
-
 }
